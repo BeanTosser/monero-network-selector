816c0173cb6ec951ccab5c38f9d2ac721ecc4352
timetherewere <mikjosjon@gmail.com>
2022-05-04 19:06:17 -0400

    Import react into typedef file to eliminate missing JSX namespace..
    .. error.

--- src/monero-network-selector.d.ts
+++ src/monero-network-selector.d.ts
@@ -1,3 +1,5 @@
+import React from "react";
+
 /*
  * This type definition has not yet been submitted to definitelyTyped;
  * Therefore, it is made available here until incorporated into the 
@@ -5,7 +7,6 @@
  */
 
 declare module 'monero-network-selector' {
-  
   export function MoneroNetworkSelector(props: MoneroNetworkSelectorProps): JSX.Element;
   export type MoneroNetworkSelectorProps = {
     networkTypes?: number[],
